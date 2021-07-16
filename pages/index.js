d1e549ed27d070ed04e8e1242b13a95f75a852f2
cedricrenouleau <ced241@gmail.com>
2021-07-16 14:14:58 +0200

    pre-rendering and data fetching

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import Head from 'next/head'
 import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Alert from '../components/alert'
+import {getSortedPostsDataFromFileSystem} from '../lib/posts'
 
-const Home = () => {
+export async function getStaticProps() {
+    const allPostsDataFromFileSystem = getSortedPostsDataFromFileSystem()
+    return {
+        props: {
+            allPostsDataFromFileSystem
+        }
+    }
+}
+
+const Home = ({allPostsDataFromFileSystem}) => {
     return (
         <Layout home>
             <Head>
@@ -17,6 +27,15 @@ const Home = () => {
 
                 <Alert type={'error'}>ERROR</Alert>
                 <Alert type={'success'}>SUCCESS</Alert>
+            </section>
+
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog posts from file system !</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsDataFromFileSystem.map(({id, date, title}) => (
+                        <li className={utilStyles.listItem} key={id}>{title} <br/> {id} <br/> {date}</li>
+                    ))}
+                </ul>
             </section>
         </Layout>
     )
